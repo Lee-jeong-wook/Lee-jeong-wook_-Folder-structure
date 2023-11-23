026a465d275cce62d46da607a28a4bfb2b72df38
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-11-23 18:49:35 +0900

    첫 커밋

---
+++ src/routes/home/home.ctrl.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const output = {
+    // home : (req, res) => {
+    //     res.render('home/index')
+    // }
+}
+
+//post방식으로 보낼 코드
+const process = {
+    // list : (req, res) => {
+    //     const data = req.body;
+    //     console.log(data);
+    //     return res.json({data : data});
+    // }
+};
+
+
+module.exports = {
+    output,
+    process
+}
